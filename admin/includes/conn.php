1db733637c8dc876e9f1919421a2311e6d717f1d
Mohshina Akter Ritu <2004040@student.ruet.ac.bd>
2025-04-12 22:48:39 +0600

    Delete admin directory

--- admin/includes/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$conn = new mysqli('localhost', 'root', '', 'libsystem', 3307);
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
--- admin/includes/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-     
-    </div>
-    <strong>&copy; 2018 - Library Management System | Brought To You By <a href="https://code-projects.org/">Code-Projects</a></strong>
-</footer>
